54edad222512ea20120794ee78450dcad1474990
Animesh Paul <ani.nitmz@gmail.com>
2020-11-24 22:11:28 -0700

    Added Immutable Class Challenge

--- Collections/Java-Collections-Immutable-Classes-Source-code/src/com/timbuchalka/Main.java
+++ Collections/Java-Collections-Immutable-Classes-Source-code/src/com/timbuchalka/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+        Map<String, Integer> tempExit;
         locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", null));
 
         tempExit = new HashMap<String, Integer>();
