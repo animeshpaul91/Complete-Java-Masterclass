61a9c633aea72f1cffe8102f7729b211cb0453ea
Animesh Paul <ani.nitmz@gmail.com>
2022-02-06 23:24:26 -0700

    Continued for 2mrw

---
+++ DropWizard/dropwizard-core/src/main/java/io/dropwizard/dropwizardConfiguration.java
@@ -0,0 +1,34 @@
+package io.dropwizard;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotEmpty;
+
+public class dropwizardConfiguration extends Configuration {
+    // TODO: implement service configuration
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+}
