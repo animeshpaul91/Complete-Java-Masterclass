8bd3ae355d0d2bbef43c8c835cb3794128ae4633
Animesh Paul <ani.nitmz@gmail.com>
2022-02-21 20:33:05 -0700

    Reviewed Testing Exceptions

--- Mockito_and_JUnit/MockitoTutorialForBeginners/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ Mockito_and_JUnit/MockitoTutorialForBeginners/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -24,7 +24,7 @@ public class ArraysCompareTest {
 
     @Test(timeout = 100)
     public void testSort_Performance() {
-        int array[] = {12, 23, 4};
+        int[] array = {12, 23, 4};
         for (int i = 1; i <= 1000000; i++) {
             array[0] = i;
             Arrays.sort(array);
