e20e5c0e91978d6038e7105955644bcc6f19561b
Animesh Paul <ani.nitmz@gmail.com>
2022-02-21 20:26:44 -0700

    Reviewed Testing Exceptions

--- Mockito_and_JUnit/MockitoTutorialForBeginners/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ Mockito_and_JUnit/MockitoTutorialForBeginners/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -1,35 +1,33 @@
 package com.in28minutes.junit.helper;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.util.Arrays;
 
-import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
 
 public class ArraysCompareTest {
 
-	@Test
-	public void testArraySort_RandomArray() {
-		int[] numbers = { 12, 3, 4, 1 };
-		int[] expected = { 1, 3, 4, 12 };
-		Arrays.sort(numbers);
-		assertArrayEquals(expected, numbers);
-	}
+    @Test
+    public void testArraySort_RandomArray() { // testing two Arrays. This matches the contents and not the object in memory
+        int[] numbers = {12, 3, 4, 1};
+        int[] expected = {1, 3, 4, 12};
+        Arrays.sort(numbers);
+        assertArrayEquals(expected, numbers);
+    }
 
-	@Test(expected=NullPointerException.class)
-	public void testArraySort_NullArray() {
-		int[] numbers = null;
-		Arrays.sort(numbers);
-	}
-	
-	@Test(timeout=100)
-	public void testSort_Performance(){
-		int array[] = {12,23,4};
-		for(int i=1;i<=1000000;i++)
-		{
-			array[0] = i;
-			Arrays.sort(array);
-		}
-	}
+    @Test(expected = NullPointerException.class) // we're expecting a null-pointer exception, always
+    public void testArraySort_NullArray() {
+        int[] numbers = null;
+        Arrays.sort(numbers);
+    }
 
+    @Test(timeout = 100)
+    public void testSort_Performance() {
+        int array[] = {12, 23, 4};
+        for (int i = 1; i <= 1000000; i++) {
+            array[0] = i;
+            Arrays.sort(array);
+        }
+    }
 }
