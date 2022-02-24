cc53352f38bc13cf49b9c837c36585fa120b1a94
Animesh Paul <ani.nitmz@gmail.com>
2022-02-23 22:56:55 -0700

    Refactored Unit Test

--- Maven_Crash_Course/productparent/productservices/src/test/java/com/bharath/product/dao/ProductDAOImplTest.java
+++ Maven_Crash_Course/productparent/productservices/src/test/java/com/bharath/product/dao/ProductDAOImplTest.java
@@ -8,17 +8,18 @@ import static org.junit.Assert.assertNotNull;
 
 public class ProductDAOImplTest {
 
-	@Test
-	public void createShouldCreateAProduct() {
-		ProductDAO dao = new ProductDAOImpl();
-		Product product = new Product();
-		product.setId(1);
-		product.setName("Ipad");
-		product.setDescription("Its Awesome!!");
-		product.setPrice(800);
-		dao.create(product);
-		Product result = dao.read(1);
-		assertNotNull(result);
-		assertEquals("Ipad", result.getName());
-	}
+    @Test
+    public void createShouldCreateAProduct() {
+        ProductDAO dao = new ProductDAOImpl();
+        Product product = new Product();
+        String productKey = "IPhone";
+        product.setId(1);
+        product.setName(productKey);
+        product.setDescription("Its Awesome!!");
+        product.setPrice(800);
+        dao.create(product);
+        Product result = dao.read(1);
+        assertNotNull(result);
+        assertEquals(productKey, result.getName());
+    }
 }
