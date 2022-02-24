aa81b08f33a649d547e02dd9675d10ae6a510bd5
Animesh Paul <ani.nitmz@gmail.com>
2022-02-23 22:41:47 -0700

    Updated DAO and DTO Objects

--- Maven_Crash_Course/productparent/productservices/src/main/java/com/bharath/product/dao/ProductDAO.java
+++ Maven_Crash_Course/productparent/productservices/src/main/java/com/bharath/product/dao/ProductDAO.java
@@ -3,12 +3,8 @@ package com.bharath.product.dao;
 import com.bharath.product.dto.Product;
 
 public interface ProductDAO {
-
 	void create(Product product);
-
 	Product read(int id);
-
 	void update(Product product);
-
 	void delete(int id);
 }
--- Maven_Crash_Course/productparent/productservices/src/main/java/com/bharath/product/dto/Product.java
+++ Maven_Crash_Course/productparent/productservices/src/main/java/com/bharath/product/dto/Product.java
@@ -1,41 +1,40 @@
 package com.bharath.product.dto;
 
-public class Product {
-	private int id;
-	private String name;
-	private String description;
-	private int price;
+public class Product { // Our Main Data Transfer Object
+    private int id;
+    private String name;
+    private String description;
+    private int price;
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public int getPrice() {
-		return price;
-	}
+    public int getPrice() {
+        return price;
+    }
 
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
