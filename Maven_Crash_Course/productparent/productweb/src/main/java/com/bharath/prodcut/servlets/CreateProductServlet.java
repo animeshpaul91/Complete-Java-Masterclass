db67c1414c50044437f390091727eff6c6a1aa69
Animesh Paul <ani.nitmz@gmail.com>
2022-02-24 20:57:47 -0700

    Reviewed Create Product Servlet

--- Maven_Crash_Course/productparent/productweb/src/main/java/com/bharath/prodcut/servlets/CreateProductServlet.java
+++ Maven_Crash_Course/productparent/productweb/src/main/java/com/bharath/prodcut/servlets/CreateProductServlet.java
@@ -18,7 +18,7 @@ public class CreateProductServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         int id = Integer.parseInt(request.getParameter("id"));
