ff5eff940f70926c56705cf88d7f4961db961c6b
Animesh Paul <ani.nitmz@gmail.com>
2022-02-28 20:48:26 -0700

    Revisited verifying calls on Mocks with Argument Capture

--- MockitoTutorialForBeginners/src/test/java/com/in28minutes/business/TodoBusinessImplMockitoTest.java
+++ MockitoTutorialForBeginners/src/test/java/com/in28minutes/business/TodoBusinessImplMockitoTest.java
@@ -12,6 +12,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 public class TodoBusinessImplMockitoTest {
@@ -62,14 +63,55 @@ public class TodoBusinessImplMockitoTest {
     }
 
     @Test
+    public void letsTestDeleteNowUsingBDDStyle() {
+        // Given
+        TodoService todoService = mock(TodoService.class);
+        List<String> allTodos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+        given(todoService.retrieveTodos("Ranga")).willReturn(allTodos);
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+        // When
+        todoBusinessImpl.deleteTodosNotRelatedToSpring("Ranga");
+        // Then
+        then(todoService).should().deleteTodo("Learn to Dance"); // verifying deleteTodo was called on todoService with arg - "Learn to Dance"
+        then(todoService).should(never()).deleteTodo("Learn Spring MVC"); // verifying that this was never called
+        then(todoService).should(never()).deleteTodo("Learn Spring"); // verifying that this was never called
+        then(todoService).should(times(1)).deleteTodo("Learn to Dance"); // 1 is the number of times
+        then(todoService).should(atLeastOnce()).deleteTodo("Learn to Dance");
+        then(todoService).should(atLeast(1)).deleteTodo("Learn to Dance"); // same thing as line 62
+        // verifying calls are required when services(dependencies) do not return values and tests need to test "side effects".
+    }
+
+    @Test
     public void captureArgument() {
+        // Declare Argument Captor
         ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+
         TodoService todoService = mock(TodoService.class);
         List<String> allTodos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
         Mockito.when(todoService.retrieveTodos("Ranga")).thenReturn(allTodos);
         TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
         todoBusinessImpl.deleteTodosNotRelatedToSpring("Ranga");
-        Mockito.verify(todoService).deleteTodo(argumentCaptor.capture());
-        assertEquals("Learn to Dance", argumentCaptor.getValue());
+
+        // Define Argument Captor
+        then(todoService).should().deleteTodo(argumentCaptor.capture());
+        // verify(todoService).deleteTodo(argumentCaptor.capture()); // same thing as previous line
+        assertThat(argumentCaptor.getValue(), is("Learn to Dance"));
+    }
+
+    @Test
+    public void captureMultipleArguments() {
+        // Declare Argument Captor
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+
+        TodoService todoService = mock(TodoService.class);
+        List<String> allTodos = Arrays.asList("Learn to Rock and Roll", "Learn Spring", "Learn to Dance");
+        Mockito.when(todoService.retrieveTodos("Ranga")).thenReturn(allTodos);
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+        todoBusinessImpl.deleteTodosNotRelatedToSpring("Ranga");
+
+        // Define Argument Captor
+        then(todoService).should(times(2)).deleteTodo(argumentCaptor.capture()); // default value is 1 without times
+        // verify(todoService).deleteTodo(argumentCaptor.capture()); // same thing as previous line
+        assertThat(argumentCaptor.getAllValues().size(), is(2));
     }
 }
