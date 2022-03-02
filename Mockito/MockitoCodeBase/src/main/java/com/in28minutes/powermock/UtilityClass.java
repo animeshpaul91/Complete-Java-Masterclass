1be21ac9715a4ef1ab9cb4b35a68caa6aed71501
Animesh Paul <ani.nitmz@gmail.com>
2022-03-01 23:30:06 -0700

    Mocked Static methods with PowerMockito

--- Mockito/MockitoCodeBase/src/main/java/com/in28minutes/powermock/UtilityClass.java
+++ Mockito/MockitoCodeBase/src/main/java/com/in28minutes/powermock/UtilityClass.java
@@ -1,9 +1,8 @@
 package com.in28minutes.powermock;
 
 public class UtilityClass {
-	static int staticMethod(long value) {
-		// Some complex logic is done here...
-		throw new RuntimeException(
-				"I dont want to be executed. I will anyway be mocked out.");
-	}
+    public static int staticMethod(long value) {
+        // Some complex logic is done here...
+        throw new RuntimeException("I dont want to be executed. I will anyway be mocked out.");
+    }
 }
