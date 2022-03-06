4596ccb1360579d2787f86132afb64e4ee08bd5c
Animesh Paul <ani.nitmz@gmail.com>
2022-03-05 22:06:23 -0700

    Reviewed Optional map and flatMap

--- Functional_Programming/src/com/Animesh/Java/FunctionalProgramming/data/Student.java
+++ Functional_Programming/src/com/Animesh/Java/FunctionalProgramming/data/Student.java
@@ -2,6 +2,7 @@ package com.Animesh.Java.FunctionalProgramming.data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Student {
     private String name;
@@ -10,6 +11,7 @@ public class Student {
     private String gender;
     private List<String> activities = new ArrayList<>();
     private int noteBooks;
+    private Optional<Bike> bike = Optional.empty();
 
     public int getNoteBooks() {
         return noteBooks;
@@ -76,6 +78,14 @@ public class Student {
         this.gpa = gpa;
     }
 
+    public Optional<Bike> getBike() {
+        return bike;
+    }
+
+    public void setBike(Optional<Bike> bike) {
+        this.bike = bike;
+    }
+
     public void printListOfActivities() {
         System.out.println(this.activities);
     }
@@ -101,6 +111,8 @@ public class Student {
                 ", gpa=" + gpa +
                 ", gender='" + gender + '\'' +
                 ", activities=" + activities +
+                ", noteBooks=" + noteBooks +
+                ", bike=" + bike +
                 '}';
     }
 }
