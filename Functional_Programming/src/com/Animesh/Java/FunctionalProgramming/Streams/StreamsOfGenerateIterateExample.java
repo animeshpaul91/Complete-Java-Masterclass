7a473ae6aa1b29547f11a790d1fbb4b4ecf5b1f4
Animesh Paul <ani.nitmz@gmail.com>
2022-03-06 18:10:23 -0700

    Started Numeric Streams

--- Functional_Programming/src/com/Animesh/Java/FunctionalProgramming/Streams/StreamsOfGenerateIterateExample.java
+++ Functional_Programming/src/com/Animesh/Java/FunctionalProgramming/Streams/StreamsOfGenerateIterateExample.java
@@ -9,17 +9,17 @@ import static java.util.stream.Collectors.toList;
 
 public class StreamsOfGenerateIterateExample {
     public static void main(String[] args) {
-        Stream<String> stringStream = Stream.of("adam", "dan", "jenny", "dave");
+        Stream<String> stringStream = Stream.of("adam", "dan", "jenny", "dave"); // create stream of a set of items
         stringStream.forEach(System.out::println);
 
-        List<Integer> integerList = Stream.iterate(1, x -> x * 2)
+        List<Integer> integerList = Stream.iterate(1, x -> x * 2) // iterate used to create infinite streams
                 .limit(10)
                 .collect(toList());
 
         System.out.println("iterate : " + integerList);
         Supplier<Integer> supplier = () -> new Random().nextInt(10);
 
-        List<Integer> integerList1 = Stream.generate(supplier)
+        List<Integer> integerList1 = Stream.generate(supplier) // generate used to create infinite streams
                 .limit(10)
                 .collect(toList());
 
