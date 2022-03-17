979bcec37946b0b023bde32cb8bdd80dadbb5b2e
Animesh Paul <ani.nitmz@gmail.com>
2022-03-16 23:23:55 -0600

    Assignment Solution

--- Futures/CompletableFuturesv2.0/parallel-asynchronous-using-java/src/main/java/com/learnjava/parallelstreams/ParallelStreamExample.java
+++ Futures/CompletableFuturesv2.0/parallel-asynchronous-using-java/src/main/java/com/learnjava/parallelstreams/ParallelStreamExample.java
@@ -24,6 +24,10 @@ public class ParallelStreamExample {
                 .collect(Collectors.toList());
     }
 
+    public List<String> string_toLowercase(List<String> namesList) {
+        return namesList.parallelStream().map(String::toLowerCase).collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         List<String> namesList = DataSet.namesList();
         ParallelStreamExample parallelStreamExample = new ParallelStreamExample();
@@ -31,5 +35,6 @@ public class ParallelStreamExample {
         List<String> result = parallelStreamExample.stringTransform(namesList, true);
         timeTaken();
         log("Result: " + result);
+        log("Assignment Solution: " + parallelStreamExample.string_toLowercase(namesList));
     }
 }
