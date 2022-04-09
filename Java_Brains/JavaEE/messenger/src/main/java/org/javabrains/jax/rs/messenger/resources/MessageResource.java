92852187c3662fb35e67f6ed9bf59d8d4e4d4fdb
Animesh Paul <ani.nitmz@gmail.com>
2022-04-09 17:29:52 -0600

    Added PUT and DELETE Methods

--- Java_Brains/JavaEE/messenger/src/main/java/org/javabrains/jax/rs/messenger/resources/MessageResource.java
+++ Java_Brains/JavaEE/messenger/src/main/java/org/javabrains/jax/rs/messenger/resources/MessageResource.java
@@ -8,6 +8,8 @@ import org.javabrains.jax.rs.messenger.service.MessageService;
 import java.util.List;
 
 @Path("/messages") // All controller methods in this class will have this prefix
+@Consumes(MediaType.APPLICATION_JSON) // This represents the body of the request
+@Produces(MediaType.APPLICATION_JSON) // This is the return type to the HTTP request
 public class MessageResource {
 
     private final MessageService messageService;
@@ -17,22 +19,31 @@ public class MessageResource {
     }
 
     @GET // This is a GET request and will resolve to /messages
-    @Produces(MediaType.APPLICATION_JSON) // This is the return type to the HTTP request
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
 
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessageInstance(@PathParam("messageId") long messageId) {
         return messageService.getMessage(messageId);
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message addMessage(Message message) {
         return messageService.addMessage(message);
+    }
+
+    @PUT
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+        message.setId(messageId);
+        return messageService.updateMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}")
+    public void deleteMessage(@PathParam("messageId") long messageId) {
+        messageService.deleteMessage(messageId);
     }
 }
