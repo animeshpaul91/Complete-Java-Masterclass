852458b116a3aa377e129f1835ace07f154a6ef1
Animesh Paul <ani.nitmz@gmail.com>
2022-04-09 16:25:32 -0600

    Added GET Message Instance Endpoint

--- Java_Brains/JavaEE/messenger/src/main/java/org/javabrains/jax/rs/messenger/resources/MessageResource.java
+++ Java_Brains/JavaEE/messenger/src/main/java/org/javabrains/jax/rs/messenger/resources/MessageResource.java
@@ -2,6 +2,7 @@ package org.javabrains.jax.rs.messenger.resources;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.javabrains.jax.rs.messenger.model.Message;
@@ -22,5 +23,12 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_XML) // This is the return type to the HTTP request
     public List<Message> getMessages() {
         return messageService.getAllMessages();
+    }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessageInstance(@PathParam("messageId") long messageId) {
+        return messageService.getMessage(messageId);
     }
 }
