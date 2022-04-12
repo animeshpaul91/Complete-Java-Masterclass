cd9c38473f3b7d971c49e6c69236dcbd4e3d6f59
Animesh Paul <ani.nitmz@gmail.com>
2022-04-11 23:43:44 -0600

    Revisited Singleton resource

--- Java_Brains/JavaEE/advanced-jaxrs/src/main/java/org/animesh/javabrains/rest/MyResource.java
+++ Java_Brains/JavaEE/advanced-jaxrs/src/main/java/org/animesh/javabrains/rest/MyResource.java
@@ -1,16 +1,21 @@
 package org.animesh.javabrains.rest;
 
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/test")
+@Singleton // this annotation will only create a single instance of MyResource class for every instance of a running application
 public class MyResource {
+
+    private int count;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String testMethod() {
-        return "It Works";
+        count++;
+        return "It Works! This method was called: " + count + " times";
     }
 }
