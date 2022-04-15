5991b4ffc55171c6e28e0dae0a4b899844b09c87
Animesh Paul <ani.nitmz@gmail.com>
2022-04-14 23:46:31 -0600

    Made POST Request

--- Java_Brains/JavaEE/messenger/src/main/java/org/javabrains/jax/rs/messenger/restclient/GETRestApiClient.java
+++ Java_Brains/JavaEE/messenger/src/main/java/org/javabrains/jax/rs/messenger/restclient/GETRestApiClient.java
@@ -1,14 +1,13 @@
 package org.javabrains.jax.rs.messenger.restclient;
 
 import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Invocation.Builder;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.javabrains.jax.rs.messenger.model.Message;
 
-public class RestApiClient {
+public class GETRestApiClient {
 
     private static final String BASE_URL = "http://localhost:8080/messenger/webapi";
 
@@ -22,28 +21,40 @@ public class RestApiClient {
     }
 
     private static String restCallFluentAPIStyle(Client client, String clientUrl) {
-        return client.target(clientUrl)
+        Response getResponse = client.target(clientUrl)
                 .request(MediaType.APPLICATION_JSON) // tells the server that the client is expecting a JSON response.
-                .get(String.class); // handy for debugging
+                .get();// handy for debugging
+
+        if (getResponse.getStatus() != 200) {
+            System.err.println("GET Request Failed");
+            return null;
+        }
+        return getResponse.readEntity(String.class); // String-ify-ing handy for debugging
     }
 
     private static Message getSingleMessageResponseObject(WebTarget webtarget, int messageId) {
-        return webtarget
+        Response getResponse = webtarget
                 .resolveTemplate("messageId", messageId) // resolves the pathVariable with the argument messageId
                 .request(MediaType.APPLICATION_JSON) // tells the server that the client is expecting a JSON response.
-                .get(Message.class); // handy for debugging
+                .get();
+
+        if (getResponse.getStatus() != 200) {
+            System.err.println("GET Request Failed");
+            return null;
+        }
+
+        return getResponse.readEntity(Message.class);
     }
 
     public static void main(String[] args) {
-        Client client = ClientBuilder.newClient(); // with this client instance you can make multiple Rest-API calls
+        Client client = WebTargetUtil.getClient(); // with this client instance you can make multiple Rest-API calls
         String clientUrl = BASE_URL + "/messages/1";
         //System.out.println(demonstrateRestClientStepByStep(client, clientUrl));
         String jsonResponse = restCallFluentAPIStyle(client, clientUrl);
         //System.out.println(jsonResponse);
 
         // Best Practices
-        WebTarget baseTarget = client.target(BASE_URL);
-        WebTarget messagesTarget = baseTarget.path("/messages");
+        WebTarget messagesTarget = WebTargetUtil.getMessagesWebTarget();
         WebTarget singleMessageTarget = messagesTarget.path("/{messageId}");
         Message message1 = getSingleMessageResponseObject(singleMessageTarget, 1);
         System.out.println(message1.getMessage());
