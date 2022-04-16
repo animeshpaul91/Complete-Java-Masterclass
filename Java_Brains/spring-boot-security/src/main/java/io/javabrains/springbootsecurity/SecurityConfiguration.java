714d8a479991cce3efa6b2097b635ab99c51902d
Animesh Paul <ani.nitmz@gmail.com>
2022-04-16 00:42:47 -0600

    Configured Spring Security Auth with H2 DB

--- Java_Brains/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
+++ Java_Brains/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,19 +9,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity // enable security for incoming web requests
+import javax.sql.DataSource;
+
+@EnableWebSecurity // enable security for incoming web requests. Spring sends in the AuthenticationManagerBuilder here
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource; // Spring automatically wires H2 datasource when H2 is on the classpath
+
+    @Autowired
+    public SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception { // spring security passes in AuthenticationManagerBuilder
         // Set your configuration on the auth object
-        auth.inMemoryAuthentication()
-                .withUser("blah")
-                .password("blah")
-                .roles("USER")
-                .and()
-                .withUser("foo")
-                .password("foo")
-                .roles("ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,enabled "
+                        + "from users "
+                        + "where username = ?") // lines 29-34 only need to be written for custom tables. These are default values
+                .authoritiesByUsernameQuery("select username,authority "
+                        + "from authorities "
+                        + "where username = ?");
     }
 
     @Bean
