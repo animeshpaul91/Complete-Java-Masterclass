891d78161e2cd52b33bd726f3d86be607d645547
Animesh Paul <ani.nitmz@gmail.com>
2022-12-06 00:06:16 -0800

    UUID Generator

--- Functional_Programming/src/com/Animesh/Java/FunctionalProgramming/UUID/UUIDGenerator.java
+++ Functional_Programming/src/com/Animesh/Java/FunctionalProgramming/UUID/UUIDGenerator.java
@@ -3,13 +3,14 @@ package com.Animesh.Java.FunctionalProgramming.UUID;
 import java.util.UUID;
 
 public class UUIDGenerator {
+    private static final String EVENT_SID_PREFIX = "EV";
     public static void main(String[] args) {
         System.out.println(getEventID());
     }
 
     private static String getEventID() {
         final String uuid = UUID.randomUUID().toString().replace("-", "");
-        final var result = "EV" + uuid;
+        final var result = EVENT_SID_PREFIX + uuid;
         System.out.println(result.length());
         return result;
     }
