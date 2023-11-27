8103a14dfeca4356b397f736b89a13075f65703d
Animesh Paul <ani.nitmz@gmail.com>
2023-11-26 23:29:53 -0800

    Added Records

---
+++ Modern_Java/modern-java/explore-latest-java-features/src/main/java/com/modernjava/records/Product.java
@@ -0,0 +1,7 @@
+package com.modernjava.records;
+
+import java.math.BigDecimal;
+
+public record Product(String name, BigDecimal cost, String type) {
+
+}
--- Modern_Java/modern-java/explore-latest-java-features/src/test/java/com/modernjava/record/ProductTest.java
+++ Modern_Java/modern-java/explore-latest-java-features/src/test/java/com/modernjava/record/ProductTest.java
@@ -1,14 +1,28 @@
 package com.modernjava.record;
 
+import com.modernjava.records.Product;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ProductTest {
 
-  
-
+    private static final Logger LOG = LoggerFactory.getLogger(ProductTest.class);
+    @Test
+    void createProduct() {
+        // given
+        final var name = "IPhone";
+        final var cost = new BigDecimal("999.999");
+        final var type = "Electronics";
+        final Product product = new Product(name, cost, type);
+        // when
+        LOG.info(() -> "product = " + product);
+        assertEquals(name, product.name());
+        assertEquals(cost, product.cost());
+        assertEquals(type, product.type());
+    }
 }
