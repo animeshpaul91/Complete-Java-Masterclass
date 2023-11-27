adf34e24c6769103a1f1999f5db014def6887ab4
Animesh Paul <ani.nitmz@gmail.com>
2023-11-26 21:23:02 -0800

    Added Enhanced Switch

--- Modern_Java/modern-java/explore-latest-java-features/src/main/java/com/modernjava/enhancedswitch/DaysInMonth.java
+++ Modern_Java/modern-java/explore-latest-java-features/src/main/java/com/modernjava/enhancedswitch/DaysInMonth.java
@@ -4,7 +4,7 @@ import java.time.Month;
 import java.time.Year;
 
 public class DaysInMonth {
-    public static int getDays(Month month, int year) {
+    public static int getDaysUsingOldSwitch(Month month, int year) {
         int noOfDays = 0;
         switch (month) {
             case SEPTEMBER:
@@ -25,5 +25,30 @@ public class DaysInMonth {
         }
         return noOfDays;
 
+    }
+
+    public static int getDaysUsingNewSwitch(Month month, int year) {
+        return switch (month) {
+            case SEPTEMBER, APRIL, JUNE, NOVEMBER -> 30;
+            // case FEBRUARY -> Year.isLeap(year) ? 29 : 28;
+            case FEBRUARY -> {
+                System.out.println("Checking if year = " + year + " is a leap year");
+                yield Year.isLeap(year) ? 29 : 28;
+            }
+            default -> 31;
+        };
+    }
+
+    public static int getDaysUsingExhaustiveSwitch(Month month, int year) {
+        return switch (month) {
+            case SEPTEMBER, APRIL, JUNE, NOVEMBER -> 30;
+            // case FEBRUARY -> Year.isLeap(year) ? 29 : 28;
+            case FEBRUARY -> {
+                System.out.println("Checking if year = " + year + " is a leap year");
+                yield Year.isLeap(year) ? 29 : 28;
+            }
+            case JANUARY, MARCH, MAY, JULY, AUGUST, OCTOBER, DECEMBER -> 31;
+            // enhanced switch replaces default if all cases are explicitly provided. Compiler does not complain
+        };
     }
 }
--- Modern_Java/modern-java/explore-latest-java-features/src/test/java/com/modernjava/enhancedswitch/DaysInMonthTest.java
+++ Modern_Java/modern-java/explore-latest-java-features/src/test/java/com/modernjava/enhancedswitch/DaysInMonthTest.java
@@ -2,9 +2,8 @@ package com.modernjava.enhancedswitch;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import com.modernjava.enhancedswitch.DaysInMonth;
+
 import java.time.Month;
 import java.util.stream.Stream;
 
@@ -14,8 +13,22 @@ class DaysInMonthTest {
 
     @ParameterizedTest
     @MethodSource("input")
-    void getDays(Month month, int expectedNoOfDays) {
-        int days = DaysInMonth.getDays(month, 2023);
+    void getDaysUsingOldSwitch(Month month, int expectedNoOfDays) {
+        int days = DaysInMonth.getDaysUsingOldSwitch(month, 2023);
+        assertEquals(expectedNoOfDays, days);
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    void getDaysUsingNewSwitch(Month month, int expectedNoOfDays) {
+        int days = DaysInMonth.getDaysUsingNewSwitch(month, 2023);
+        assertEquals(expectedNoOfDays, days);
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    void getDaysUsingExhaustiveSwitch(Month month, int expectedNoOfDays) {
+        int days = DaysInMonth.getDaysUsingExhaustiveSwitch(month, 2023);
         assertEquals(expectedNoOfDays, days);
     }
 
