a93417e45b638532808e9709b63afbb85438f818
Animesh Paul <ani.nitmz@gmail.com>
2024-02-11 20:15:27 -0800

    Added Custom Constructors and Validations

--- Modern_Java/modern-java/explore-latest-java-features/src/test/java/com/modernjava/record/ProductTest.java
+++ Modern_Java/modern-java/explore-latest-java-features/src/test/java/com/modernjava/record/ProductTest.java
@@ -8,6 +8,7 @@ import org.junit.platform.commons.logging.LoggerFactory;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ProductTest {
 
@@ -24,5 +25,45 @@ class ProductTest {
         assertEquals(name, product.name());
         assertEquals(cost, product.cost());
         assertEquals(type, product.type());
+    }
+
+    @Test
+    void createProductNameNotValid() {
+        // given
+        final var cost = new BigDecimal("999.999");
+        final var type = "Electronics";
+
+        // when
+        final var exception = assertThrows(IllegalArgumentException.class,
+                () -> new Product("", cost, type));
+
+        // then
+        assertEquals("name is invalid", exception.getMessage());
+    }
+
+    @Test
+    void createProductCostNotValid() {
+        // given
+        final var name = "IPhone";
+        final var cost = new BigDecimal("-999.999");
+        final var type = "Electronics";
+
+        // when
+        final var exception = assertThrows(IllegalArgumentException.class,
+                () -> new Product(name, cost, type));
+
+        // then
+        assertEquals("cost is invalid", exception.getMessage());
+    }
+
+    @Test
+    void createProductCustomConstructor() {
+        // given
+        final var name = "IPhone";
+        final var cost = new BigDecimal("999.999");
+        final Product product = new Product(name, cost);
+        // when
+        LOG.info(() -> "product = " + product);
+        assertEquals("General", product.type());
     }
 }
