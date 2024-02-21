69ebc3fa705c47efa5aaf72aaa0097c9b548c3b1
Animesh Paul <ani.nitmz@gmail.com>
2024-02-20 21:09:07 -0800

    Configured Wiremock

--- wiremock-for-java-developers/movies-api-client/src/test/java/com/learnwiremock/service/MoviesClientTest.java
+++ wiremock-for-java-developers/movies-api-client/src/test/java/com/learnwiremock/service/MoviesClientTest.java
@@ -1,15 +1,23 @@
 package com.learnwiremock.service;
 
+import com.github.jenspiegsa.wiremockextension.ConfigureWireMock;
+import com.github.jenspiegsa.wiremockextension.InjectServer;
+import com.github.jenspiegsa.wiremockextension.WireMockExtension;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+import com.github.tomakehurst.wiremock.core.Options;
 import com.learnwiremock.dto.Movie;
 import com.learnwiremock.exceptions.MovieErrorResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
 import java.time.Month;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -17,9 +25,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
+@ExtendWith(WireMockExtension.class)
 class MoviesClientTest {
 
     private MoviesClient moviesClient;
+
+    @InjectServer
+    private WireMockServer wireMockServer;
+
+    @ConfigureWireMock
+    private final Options options = wireMockConfig()
+            .port(8088)
+            .notifier(new ConsoleNotifier(true));
 
     @BeforeEach
     void setUp() {
