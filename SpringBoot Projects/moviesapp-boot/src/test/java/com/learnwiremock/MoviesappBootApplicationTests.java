64cefbf571db381a233387d76f5c5990b1654cdf
Animesh Paul <ani.nitmz@gmail.com>
2024-02-25 20:28:41 -0800

    Initial Commit to add SpringBoot Project

---
+++ SpringBoot Projects/moviesapp-boot/src/test/java/com/learnwiremock/MoviesappBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwiremock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesappBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SpringBoot Projects/moviesapp-boot/src/test/java/com/learnwiremock/service/MoviesRestClientTest.java
@@ -0,0 +1,18 @@
+package com.learnwiremock.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+class MoviesRestClientTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
